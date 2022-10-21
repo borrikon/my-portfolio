1f231fac6d4019ee481296a807969a61dc03a193
borrikon <borrikon@gmail.com>
2022-10-21 08:18:31 +0300

    fixes

--- src/components/aboutInfo/index.js
+++ src/components/aboutInfo/index.js
@@ -14,31 +14,31 @@ function AboutInfo(){
             </figure>
             <section className='userBio'>
                 <h2 className='userBio__title'>
-                    Boris Ushnevich
+                    Boris Ushnevych
                 </h2>
                 <article className='userBio__subtitle'>
                     <p>
                         Junior front-end developer from Kiev (Ukraine).
                     </p>
                     <p>
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
+                        Hello, my name is Boris. I’ve found 
+                        myself in programming a few years ago. 
+                        At first it was hobby, then it became my job.
+                        In my work I use a wide stack of technologies 
+                        such as HTML, CSS(SCSS), JS, React.js, Redux, 
+                        Gulp, Git, npm and others.
                     </p>
                     <p>
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
-                        Lorem Lorem Lorem Lorem Lorem Lorem
+                        I love finding solutions to hard tasks 
+                        and puzzles and creating web-apps that 
+                        would be convenient for people. I’m sure 
+                        that not only clients but also developers 
+                        are satisfied if the project is high-quality. 
+                        Therefore I always improve my skills and learn 
+                        how to make code better.
                     </p>
                     <p>
-                        Hablemos!
+                        How to get in touch with me:
                     </p>
                 </article>
                 <a 
--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -17,29 +17,18 @@ function Intro(){
                 </h2>
                 <div className='info__intro--subtitle'>
                     <p>
-                        Hola! Mi nombre es Miguel 
-                        y tengo gran afinidad hacía 
-                        el trabajo colaborativo, 
-                        me gusta crear diseños que 
-                        inspiren e involucren a 
-                        las personas. Como diseñador UI, 
-                        considero que un buen producto 
-                        no solo brinda una solución sino 
-                        que permite crear una experiencia 
-                        emocional en las personas que lo 
-                        usan.
-                    </p>
-                    <p>
-                        En mi proceso creativo utilizo 
-                        herramientas como Figma, Adobe XD, 
-                        Photoshop e Illustrator que me ayudan 
-                        mucho en el flujo de trabajo para la 
-                        producción de interfaces de usuario.
+                        Hello, my name is Boris. I’ve 
+                        found myself in programming a 
+                        few years ago. At first it was hobby, 
+                        then it became my job. In my work 
+                        I use a wide stack of technologies 
+                        such as HTML, CSS(SCSS), JS, React.js, 
+                        Redux, Gulp, Git, npm and others.
                     </p>
                 </div>
                 <Link to='/about'>
                     <button className='btn btn-secondary btn-secondary--link'>
-                        Mas acerca de mi
+                        More about me
                         <img src={arrowRight} alt=''/> 
                     </button>
                 </Link>
